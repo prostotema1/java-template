54f41fb4fa4ba42b775bddfa2695e8ba52b88450
prostotema1 <st087375@student.spbu.ru>
2021-09-13 20:24:53 +0300

    Quicksort

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -3,16 +3,27 @@ package edu.spbu.sort;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-/**
- * Created by artemaliev on 07/09/15.
- */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
-  }
+  public static void qSort(int[] arr, int low, int high) {
+    int LowerBound = low, HigherBound = high;
+    int pivot = arr[(LowerBound + HigherBound) / 2];
+    int temp;
 
-  public static void sort (List<Integer> list) {
-    Collections.sort(list);
+    while(LowerBound <= HigherBound) {
+      while (arr[LowerBound] < pivot) { ++LowerBound; }
+      while (arr[HigherBound] > pivot) { --HigherBound; }
+
+      if (LowerBound <= HigherBound) {
+        temp = arr[LowerBound];
+        arr[LowerBound] = arr[HigherBound];
+        arr[HigherBound] = temp;
+        ++LowerBound;
+        --HigherBound;
+      }
+    }
+
+    if (HigherBound > low) { qSort(arr, low, HigherBound); }
+    if (LowerBound < high) { qSort(arr, LowerBound, high); }
   }
 }
+
--- src/test/java/edu/spbu/sort/IntSortTest.java
+++ src/test/java/edu/spbu/sort/IntSortTest.java
@@ -11,13 +11,11 @@ import static org.junit.Assert.*;
 /**
  * Created by artemaliev on 07/09/15.
  */
-public class IntSortTest
-{
+public class IntSortTest {
   public static final int SEED = 1;
-  public static final int ARRAY_SIZE = 10000000;
+  public static final int ARRAY_SIZE = 100000000;
 
   /**
-   *
    * @param size array size
    * @param seed for the pseudo random generator
    * @return random generated int array. It will be the same for the same seed and size.
@@ -25,7 +23,7 @@ public class IntSortTest
   int[] generateRandomIntArray(int size, long seed) {
     int array[] = new int[size];
     Random rnd = new Random(seed);
-    for (int i = 0 ; i < array.length; i++) {
+    for (int i = 0; i < array.length; i++) {
       array[i] = rnd.nextInt();
     }
     return array;
@@ -35,35 +33,13 @@ public class IntSortTest
   public void testSortArray() throws Exception {
     int array[] = generateRandomIntArray(ARRAY_SIZE, SEED);
 
-    IntSort.sort(array);
+    IntSort.qSort(array, 0, array.length - 1);
 
     // проверяем правильность сортировки
     int previousValue = Integer.MIN_VALUE;
-    for (int i = 0; i < array.length ; i++) {
-      assertTrue("Element " + array[i] + " at " + i + " position is not in the order", array[i] >= previousValue );
+    for (int i = 0; i < array.length; i++) {
+      assertTrue("Element " + array[i] + " at " + i + " position is not in the order", array[i] >= previousValue);
       previousValue = array[i];
-    }
-  }
-
-  @Test
-  public void testSortList() throws Exception {
-    int array[] = generateRandomIntArray(ARRAY_SIZE, SEED);
-    List<Integer> list = new ArrayList<Integer>(ARRAY_SIZE);
-    for (int i: array) {
-      list.add(Integer.valueOf(i));
-    }
-
-    //сортируем массив и замеряем время работы
-    long startTime = System.nanoTime();
-    IntSort.sort(list);
-    long estimatedTime = System.nanoTime() - startTime;
-    System.out.println("Execution time(ms) " + (estimatedTime/ 1000000));
-
-    // проверяем правильность сортировки
-    int previousValue = Integer.MIN_VALUE;
-    for (int i = 0; i < list.size() ; i++) {
-      assertTrue("Element " + list.get(i) + " at " + i + " position is not in the order", list.get(i) >= previousValue);
-      previousValue = list.get(i);
     }
   }
 }
